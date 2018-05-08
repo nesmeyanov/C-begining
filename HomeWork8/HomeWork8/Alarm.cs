d169f06c3d7da33722bee56da1c772b94bb70b20
nesmeyanov <nesmeyanov1990@gmail.com>
2018-05-08 22:10:54 +0300

    Added project

---
+++ HomeWork8/HomeWork8/Alarm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+
+namespace HomeWork8
+{
+    public class Alarm
+    {
+        private int hours;
+        private int minutes;
+        public event EventHandler<string> WakeUp;
+
+        public void SetAlarm(string input)
+        {
+            parse(input);
+            WakeUp.Invoke(this, $"Time is: {hours}:{minutes}");
+        }
+
+        private void parse(string input)
+        {
+            TimeSpan time;
+            if (!TimeSpan.TryParse(input, out time))
+            {
+                throw new ValidationException("parsing error");
+            }
+
+            if (time.Days > 0) // in case user input '24'
+            {
+                throw new ValidationException("parsing error");
+            }
+
+
+            hours = time.Hours;
+            minutes = time.Minutes;
+            
+        }
+    }
+}
---
+++ HomeWork8/HomeWork8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork8
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите время будильника в формате: hh:mm ");
+
+            string input=Console.ReadLine();
+
+            Alarm alarm= new Alarm();
+            
+            alarm.WakeUp += Alarm_WakeUp;
+            alarm.SetAlarm(input);
+
+            Console.ReadLine();
+
+        }
+
+        private static void Alarm_WakeUp(object sender, string e)
+        {
+            Alarm alarm = (Alarm) sender;
+            Console.WriteLine($"{e}");
+            Console.WriteLine();
+            
+        }
+    }
+}
